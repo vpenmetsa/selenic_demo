15ebd47a86aadf9eb31d6f45eaacda24e68fa4fa
vpenmetsa <vpenmetsa@192.168.0.13>
2020-04-28 09:19:56 -0400

    Added Resource into project

---
+++ Demo/src/test/resources/selenic_analyzer.bat
@@ -0,0 +1,4 @@
+@ECHO
+ECHO  Venkata! We are running this script to start to analyse the execution
+java -jar "Demo\src\test\resources\selenic_analyzer.bat\selenic_analyzer.jar" -data "C:\Users\vpenmetsa\parasoft\recorded_data" -report "Demo\Analyzer_Report\report.html"
+PAUSE
